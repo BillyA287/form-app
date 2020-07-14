f4498a537bac4903c3ed5202d54213f0a61a4b49
BillyA287 <48728966+BillyA287@users.noreply.github.com>
2020-07-14 17:55:09 +0100

    included react fragment in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 import {Form} from './components/Form/form-component'
-
+import {Fragment} from 'react'
 
 function App() {
   return (
+    <Fragment>
     <MuiThemeProvider >
       <Form />
     </MuiThemeProvider>
-    
+    </Fragment>
   );
 }
 
