816928cc1b256b202fa2ace1683c8c7bb0b5d644
BillyA287 <48728966+BillyA287@users.noreply.github.com>
2020-07-16 20:34:12 +0100

    added tests for UserDetails component

--- src/components/UserDetails/user.test.js
+++ src/components/UserDetails/user.test.js
@@ -6,22 +6,20 @@
 import React from 'react'
 import UserDetails from './user.details-component'
 import {shallow} from 'enzyme'
-// describe('UserDetails', ()=>{
-//     const wrapper = shallow(<UserDetails/>)
-//     it('renders', ()=> {
-//         expect(wrapper.contains("Enter User Details")).toEqual(true);
-//     })
-// })
+import Button from "@material-ui/core/Button";
 
-const setup = props => {
-    const component = shallow(<UserDetails {...props} />)
-    return {
-        component: component
-    }
-}
-describe('UserDetails', ()=>{
-    it('should render a form page', ()=>{
-        const {component} = setup({UserDetails: <UserDetails/>});
-        expect(component.props().UserDetails).toBeDefined()
-    })
+
+it('expect to render UserDetails component', ()=> {
+    expect(shallow(<UserDetails values={{}} handleChange={jest.fn()} />).length).toEqual(1)
 })
+
+it('to check if handlechange function executes when button is clicked', ()=> {
+    const mockCallButton = jest.fn()
+    const fakeEvent = { preventDefault: mockCallButton };
+    const wrapper = shallow(
+      <UserDetails nextStep={jest.fn()} values={{}} handleChange={jest.fn()} />
+    );
+    wrapper.find(Button).simulate("click", fakeEvent);
+    expect(mockCallButton).toHaveBeenCalled()
+})
+
